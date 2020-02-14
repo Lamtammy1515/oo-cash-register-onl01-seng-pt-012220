b6b10569e4f3a683390163108d29cf5aea0e959a
Tammy Lam <lamtammy1515@gmail.com>
2020-02-14 00:19:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,10 @@ class CashRegister
   
   def add_item(item, price, quantity = 1)
    @transaction = [name, price, quantity]
-   
-   
+   quantity.times do 
+     @items.push self.transaction[0]
+     @total += self.transaction[1]
+   end 
   end
   
   def apply_discount
