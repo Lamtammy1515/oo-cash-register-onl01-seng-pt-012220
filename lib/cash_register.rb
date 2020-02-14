a27570bdd709632e2b4c88cfe6beb62199ec7545
Tammy Lam <lamtammy1515@gmail.com>
2020-02-14 00:31:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,10 +3,9 @@ class CashRegister
   attr_accessor :total, :discount, :purchases, :transaction
  
   def initialize(discount = nil)
-    self.purchases = []
     self.total = 0
     self.discount = discount if discount != nil
-    
+    self.purchases = []
   end 
 
   
